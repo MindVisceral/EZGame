83f4da3f41ae23a56b54e6f76f743737abe45ff7
MindVisceral <MindVisceral@protonmail.com>
2024-07-12 13:53:09 +0200

    HitEffect improv., tiny changes, first weapon done

--- Resources/Entities/Weapons/Effects/Hit_Effect_Emitter.gd
+++ Resources/Entities/Weapons/Effects/Hit_Effect_Emitter.gd
@@ -1,12 +1,27 @@
+## We want the particles_colour variable to work in-Editor
+@tool
 extends Node3D
+## NOTE: This Node is queue_free()-d by the AnimationPlayer when the Trail animation is finished
+
 
 ## This GPUParticles3D Node is all that this HitEffect really is
 @onready var Particles: GPUParticles3D = $Particles
+
+## Alpha value is used for particle Transparency
+@export var particles_colour: Color = Color.YELLOW
 
 ## The Effect just doesn't happen without this. Also done for ease of editing through the Editor
 func _ready() -> void:
 	Particles.one_shot = true
 	Particles.emitting = true
+	
+	Particles.material_override.albedo_color = particles_colour
+	request_ready()
+
+## Make the Particles' albedo_color update to particles_colour every frame
+func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		Particles.material_override.albedo_color = particles_colour
 
 ## NOTE: This is called by the Node which spawns the HitEffect!
 ## NOTE: (that's either the weapon itself or the projectile bullet)
@@ -18,7 +33,3 @@ func draw_effect(pos: Vector3, normal: Vector3) -> void:
 	
 	## Make the Particles emit in the same direction as the hit object's Normals
 	Particles.process_material.direction = normal
-
-## This Node is queue_free()-d when the Particles are done
-func _on_timer_timeout() -> void:
-	queue_free()
