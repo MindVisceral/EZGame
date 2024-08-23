bc23ccd7973b84c315aa673b527daf6036289b77
MindVisceral <MindVisceral@protonmail.com>
2024-08-23 14:43:44 +0200

    Improved BulletTime w/ my TD game implementation

--- Scenes & Scripts/Autoloads/BulletTime.gd
+++ Scenes & Scripts/Autoloads/BulletTime.gd
@@ -1,46 +1,77 @@
 extends Node
 
-## Default time_scale
-var default_time_scale: float = 1.0
-## Current time_scale, set in _ready
-var time_scale: float = 1.0
-## Minimum and maximum time_scale limits
-var lower_limit: float = 0.35
-var upper_limit: float = 2.0
+###-------------------------------------------------------------------------###
+##### Changable variables
+###-------------------------------------------------------------------------###
 
-## Time it takes (in seconds) for the time_scale to reach the lower/upper limit;
+## Default time_scale. Should always be set to 1.0
+var default_time_scale: float = 1.0
+
+## The target time_scale. When time is slowed down, this is the time_scale we're aiming for.
+var target_time_scale: float = 0.35
+
+## Time (in seconds) it takes for the time_scale to reach the lower/upper limit;
 var time_until_limit: float = 0.4
+
+
+###-------------------------------------------------------------------------###
+##### Regular variables
+###-------------------------------------------------------------------------###
+
+## Current time_scale, set to be the same as default_time_scale on _ready().
+var time_scale: float = 1.0
 
 ## We only need one Tween; If we made a new one on every time, they would interfere with each other
 var bullet_time_tween: Tween
 
+## Is the game slowed down at the momet?
+var active: bool = false
+
+###-------------------------------------------------------------------------###
+##### Setup
+###-------------------------------------------------------------------------###
 
 func _ready() -> void:
 	time_scale = default_time_scale
-
-func _physics_process(delta: float) -> void:
-	## If the Player wants to be put into BulletTime mode...
-	if Input.is_action_just_pressed("input_bullet_time"):
-		
-		## If time_scale is about equal to default_time_scale time, we tween to bullet time time
-		if time_scale >= (default_time_scale - 0.05):
-			##
-			bullet_time_tween = get_tree().create_tween()
-			bullet_time_tween.tween_property(self, "time_scale", lower_limit, time_until_limit)
-		## Otherwise, we tween back to default_time_scale time
-		else:
-			bullet_time_tween = get_tree().create_tween()
-			bullet_time_tween.tween_property(self, "time_scale", default_time_scale, time_until_limit)
 	
-	## If a time_scale-changing Tween is created, we do the following...
-	if bullet_time_tween != null:
-		## We make sure the Tween's time works in real time
-		## and isn't affected by the time_scale changing
-		bullet_time_tween.set_speed_scale(1.0 / time_scale)
+
+
+###-------------------------------------------------------------------------###
+##### Core functions
+###-------------------------------------------------------------------------###
+
+## Await for input.
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("input_bullet_time"):
+		change_time_scale()
 		
-		## Limit time_scale
-		clampf(time_scale, lower_limit, upper_limit)
+	
+
+## This function is called when the Player wants to slow down the game or return to default speed
+func change_time_scale() -> void:
+	## Instantiate the Tween.
+	bullet_time_tween = get_tree().create_tween()
+	
+	## We must make sure the Tween's time works in real time;
+	## isn't affected by the time_scale changing.
+	bullet_time_tween.set_speed_scale(1.0 / time_scale)
+	
+	
+	## If the game is going at its default speed, speed it up.
+	if not active:
+		active = true
 		
-		## Finally, apply time_scale
-		Engine.time_scale = time_scale
-		AudioServer.playback_speed_scale = time_scale
+		bullet_time_tween.tween_property(Engine, "time_scale", target_time_scale, time_until_limit)
+		bullet_time_tween.tween_property(AudioServer, "playback_speed_scale", \
+			target_time_scale, time_until_limit)
+		
+	
+	## Otherwise, tween back to default_time_scale time.
+	elif active:
+		active = false
+		
+		bullet_time_tween.tween_property(Engine, "time_scale", default_time_scale, time_until_limit)
+		bullet_time_tween.tween_property(AudioServer, "playback_speed_scale", \
+			default_time_scale, time_until_limit)
+		
+	
